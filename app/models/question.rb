f52a586ec6018f7bd0c52757a8a3a56fe259bc91
nick <nickznajkowski@gmail.com>
2016-03-29 17:48:16 -0400

    fix question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,21 +1,4 @@
 class Question < ActiveRecord::Base
-  validates :title, :body, presence: true
+  validates :title, presence: true
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def index
-  end
-
-  def destroy
-  end
 end
