36074baa88471f1ab8854efad63ebda679e56d54
nick <nickznajkowski@gmail.com>
2016-03-31 17:40:10 -0400

    updated questions to have user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331210907) do
+ActiveRecord::Schema.define(version: 20160331213754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160331210907) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "details"
+    t.integer  "user_id"
   end
 
 end
