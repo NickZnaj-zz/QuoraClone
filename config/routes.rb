b3d700e8b3a226d89aa00dcb2a93d6c66330adf2
nick <nickznajkowski@gmail.com>
2016-04-03 18:05:00 -0400

    add show route to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	resources :users, only: [:new, :create, :edit, :update]
+	resources :users, only: [:new, :create, :edit, :update, :show]
 
   namespace :api, defaults: {format: :json} do
     resources :questions do
