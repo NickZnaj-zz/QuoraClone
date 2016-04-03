94170bbd21d04eb11b4899214da9bf1e671c2ce3
nick <nickznajkowski@gmail.com>
2016-04-03 16:44:47 -0400

    change seed data to include user ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,25 @@
 
 Question.create!(
   title: "can you see me?",
-  # user_id: 1
+  user_id: 1
 )
 
 Question.create!(
   title: "this is another test, what do you think?",
+	user_id: 1
 )
 Question.create!(
   title: "how is babby formed?",
+	user_id: 1
+
 )
 Question.create!(
   title: "do you even lift bro?",
+	user_id: 2
 )
 Question.create!(
   title: "am i doing this right?",
+	user_id: 2
 )
 
 Answer.create!(
@@ -32,13 +37,17 @@ Answer.create!(
 Answer.create!(
   body: "You're on the wrong site",
   question_id: 3
+	user_id: 2
 )
 
 Answer.create!(
   body: "idk check yahoo answers",
   question_id: 3
+	user_id: 1
+
 )
 Answer.create!(
   body: "They need to do way instain mother> who kill thier babbys. becuse these babby cant frigth back? it was on the news this mroing a mother in ar who had kill her three kids. they are taking the three babby back to new york too lady to rest my pary are with the father who lost his children ; i am truley sorry for your lots",
   question_id: 3
+	user_id: 2
 )
