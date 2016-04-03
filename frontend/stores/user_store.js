0d9d6c2e4c651f813cd23098db893badc5fc547d
nick <nickznajkowski@gmail.com>
2016-04-03 18:07:36 -0400

    add show method to users controller

---
+++ frontend/stores/user_store.js
@@ -0,0 +1,55 @@
+var Store = require('flux/utils').Store;
+var Dispatcher = require('../dispatcher/Dispatcher');
+
+var UserStore = new Store(Dispatcher);
+var UserConstants = require('../constants/user_constants');
+var _users = {};
+
+
+var resetUsers = function(users) {
+  _users = {};
+  users.forEach(function (user){
+    _users[user.id] = user;
+  });
+};
+
+var resetUser = function(user){
+  _users[user.id] = user;
+};
+
+var deleteUser = function(id) {
+	delete _users.id;
+};
+
+
+UserStore.all = function() {
+  var users = [];
+  for (var id in _users) {
+    users.push(_users[id]);
+  }
+  return users;
+};
+
+UserStore.find = function(id) {
+  return _users[id];
+};
+
+UserStore.__onDispatch = function (payload) {
+  switch(payload.actionType) {
+    case UserConstants.USERS_RECEIVED:
+      resetUsers(payload.users);
+      UserStore.__emitChange();
+      break;
+    case UserConstants.USER_RECEIVED:
+      resetUser(payload.user);
+      UserStore.__emitChange();
+      break;
+		case UserConstants.USER_DELETED:
+			deleteUser(payload.id);
+			UserStore.__emitChange();
+			break;
+  }
+};
+
+
+module.exports = UserStore;
