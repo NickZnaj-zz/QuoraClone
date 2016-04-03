377b2d4c4b50da7669283798c5a77f4f26044dce
nick <nickznajkowski@gmail.com>
2016-04-02 21:26:40 -0400

    set up sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,29 @@
 class Api::SessionsController < ApplicationController
+
+	def show
+		if logged_in?
+			render json: current_user
+		else
+			render json: { message: "Not Logged in" }, status: 401
+		end
+	end
+
+	def create
+		user = User.find_by_credentials(
+			params[:username],
+			params[:password]
+		)
+
+		if user && user.valid_password?(params[:password])
+			log_in!(user)
+			render json: author
+		else
+			render json: { message: "Invalid Login Credentials" }, status: 401
+		end
+	end
+
+	def destroy
+		log_out!
+		render json: {}
+	end
 end
